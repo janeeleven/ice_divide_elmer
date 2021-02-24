07bb64a89ba4abf2f141556b3f5be8cacc77ff74
root <root@elmer-instance-janie.us-central1-c.c.ldeo-glaciology.internal>
2021-02-24 22:44:40 +0000

    Accumulation Driven or Flux driven setup

---
+++ runfunc_varxa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash/
+
+###takes files with input values for accumulation gradient, accumulation, divide position, and template filename 
+
+
+####### Variables ##########
+inputfil=runvalues.txt
+outputbucket=gs://ldeo-glaciology/elmer_janie/run_outputs/
+dockcont=elmertest_janie1
+###############
+
+
+while read -r a0 DivPos filnam remainder; do
+
+mkdir -p icerise_run
+#touch Korff_run/examplefile.txt
+mkdir -p icerise_run/mesh/
+mkdir -p icerise_run/src/
+bash update_sif_vardiv.sh ${a0} ${DivPos} ${filnam} #create new sif file from template
+##cp Case_*.sif output/
+cp elmer_icerise_base/mesh.grd icerise_run #template mesh grid
+cp elmer_icerise_base/src/* icerise_run/src/ #template solvers into new folder
+cp initialcond.dat icerise_run/mesh/initialcond.dat
+mv Case_${a0}_${DivPos}.sif icerise_run/
+chmod -R 777 icerise_run/
+echo 'Entering docker container and running the script'
+sudo service docker start
+docker start ${dockcont}
+
+echo [`date +"%D %T"`] Running Case_${a0}_${DivPos} | tee -a log.txt
+echo [`date +"%D %T"`] ElmerGrid 1 2 icerise_run/mesh.grd | tee -a log.txt
+docker exec ${dockcont} ElmerGrid 1 2 shared_directory/icerise_run/mesh.grd | tee -a log.txt
+
+echo [`date +"%D %T"`] ElmerSolver shared_directory/icerise_run/Case_${a0}_${DivPos}.sif | tee -a log.txt
+docker exec ${dockcont} ElmerSolver shared_directory/icerise_run/Case_${a0}_${DivPos}.sif | tee -a log.txt
+
+#gsutil cp icerise_run/Surface.dat gs://ldeo-glaciology/elmer_janie/run_outputs/Surface_${xa}_${a0}_${DivPos}.dat
+#gsutil cp icerise_run/src/Results.dat gs://ldeo-glaciology/elmer_janie/run_outputs/Results_${xa}_${a0}_${DivPos}.dat
+gsutil cp icerise_run/mesh/*.vtu ${outputbucket}run_${a0}_${DivPos}_varaccum/
+
+echo [`date +"%D %T"`] Finished Case_${a0}_${DivPos}_varaccum | tee -a log.txt
+done < ${inputfil}
